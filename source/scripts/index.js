a230eba3cdb8b4050ab3d174ec637aca5b2e91c9
Ridge <maddy.ab410@gmail.com>
2026-09-22 23:11:49 +0300

    add responsive grid layout to all page templates (#12)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+const menu = document.querySelector('.main-nav__wrapper');
+const menuButton = document.querySelector('.main-nav__toggle');
+
+menuButton.onclick = function() {
+  menu.classList.toggle('main-nav__wrapper--closed');
+  menuButton.classList.toggle('main-nav__toggle--close');
+};
