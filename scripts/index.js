1e52abe78a06e15384ff8c3a0b95d3210bfc6b79
CreatorRidge <CreatorRidge@users.noreply.github.com>
2026-09-22 20:12:01 +0000

    Deploying to gh-pages from @ CreatorRidge/catEnergy@fbd8b6bd2fb4d039e8c8246c7bac13e13a26042f 🚀

---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".main-nav__wrapper"),e=document.querySelector(".main-nav__toggle");e.onclick=function(){n.classList.toggle("main-nav__wrapper--closed"),e.classList.toggle("main-nav__toggle--close")};
